eb2aaddabc0343ef78ca5f853d037a06c23d16ad
Shalom Daniel <daniel.sh@northeastern.edu>
2023-01-29 21:34:15 -0500

    First commit for lab2

---
+++ src/Model/Customer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Shalom Daniel
+ */
+public class Customer {
+    String fullName;
+    int customerId;
+    
+    public Customer() {
+        
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    
+    
+}
